5186b0b81c32c6369af352e0730dcd69650ed660
Eugeniy Litvinenko <evlit23@gmail.com>
2024-04-23 14:43:15 +0300

    Step 4. Added checkstyle

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,11 +2,11 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static String getMame(){
+    public static String getMame() {
         Scanner s = new Scanner(System.in);
         System.out.println("May I have your name?");
         String name = s.nextLine();
-        while (name.equals("") ) {
+        while (name.equals("")) {
             System.out.println("The name can't be blank! Enter again!");
             name = s.nextLine();
         }
