dcdfb5a3b49f6f7ed5f1b8c663d8d25d78b12fb4
Eugeniy Litvinenko <evlit23@gmail.com>
2024-04-23 13:20:47 +0300

    Step 3. Added Cli.java and Makefile

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,15 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Cli {
+    public static String getMame(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("May I have your name?");
+        String name = s.nextLine();
+        while (name.equals("") ) {
+            System.out.println("The name can't be blank! Enter again!");
+            name = s.nextLine();
+        }
+        return name;
+    }
+}
